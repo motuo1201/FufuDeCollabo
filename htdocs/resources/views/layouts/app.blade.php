07ff2e5da1d6027014dc6b172c3f4b71bfc71654
motuo1201 <enomotu@hotmail.com>
2018-01-18 15:56:55 +0900

    ナビゲーションメニューのレイアウト調整

--- htdocs/resources/views/layouts/app.blade.php
+++ htdocs/resources/views/layouts/app.blade.php
@@ -24,49 +24,45 @@
         </div>
         @else
         <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container">
-                <div class="bs-component">
-                    <nav class="navbar navbar-dark bg-danger">
-                        <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarResponsive2" 
-                                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
-                        <div class="container collapse navbar-toggleable-md" id="navbarResponsive2">
-                            <a class="navbar-brand" href="{{route('welcome')}}">トップページ</a>
-                            <ul class="nav navbar-nav">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">リンク</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">リンク</a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="http://example.com" id="supportedContentDropdown2" 
-                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">メニュー</a>
-                                    <div class="dropdown-menu" aria-labelledby="supportedContentDropdown2">
-                                        <a class="dropdown-item" href="#">高坂 穂乃果</a>
-                                        <a class="dropdown-item" href="#">南 ことり</a>
-                                        <a class="dropdown-item" href="#">園田 海未</a>
-                                    </div>
-                                </li>
-                            </ul>
-                            <ul class="nav navbar-nav form-inline float-lg-right">
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="http://example.com" id="supportedContentDropdown2" 
-                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
-                                    <div class="dropdown-menu" aria-labelledby="supportedContentDropdown2">
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();" class="dropdown-item">
-                                            ログアウト
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
+            <div class="bs-component">
+                <nav class="navbar navbar-dark bg-danger">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarResponsive2" 
+                            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"></button>
+                    <div class="container collapse navbar-toggleable-md" id="navbarResponsive2">
+                        <a class="navbar-brand" href="{{route('home')}}">トップページ</a>
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">リンク</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">リンク</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="http://example.com" id="supportedContentDropdown2" 
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">メニュー</a>
+                                <div class="dropdown-menu" aria-labelledby="supportedContentDropdown2">
+                                    <a class="dropdown-item" href="#">sample</a>
+                                </div>
+                            </li>
+                        </ul>
+                        <ul class="nav navbar-nav form-inline float-lg-right">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="http://example.com" id="supportedContentDropdown2" 
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }}</a>
+                                <div class="dropdown-menu" aria-labelledby="supportedContentDropdown2">
+                                    <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();" class="dropdown-item">
+                                        ログアウト
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </nav>
         @endguest
