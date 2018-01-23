12924e19018ceff10e80737f63ae667d8e92f60d
motuo1201 <enomotu@hotmail.com>
2018-01-23 13:56:02 +0900

    ヨメータ―の画像とピンの画像を差替え。 ピンを刺したときに、ちょっと動きをつける。

--- htdocs/resources/views/yomerter/yomerter-slide.blade.php
+++ htdocs/resources/views/yomerter/yomerter-slide.blade.php
@@ -5,7 +5,7 @@
     <h6>今の気分は？</h6>
     <div class="yomerter" id="yomerter">
         <img src="./img/yometer_face.png" alt="ヨメータ―" class="yomerter">
-        <img src="./img/pushpin.png" alt="ピン" class="pin" id="pin">
+        <img src="./img/pushpin.png" alt="ピン" class="pin animate-pin" id="pin">
     </div>
     <div class="row">
         <div>
@@ -16,6 +16,8 @@
     <div class="row" style="padding-top: 20px">
         <div class="col-md-12">
             <input type="submit" class="btn btn-block btn-success" value="パートナーに思いを伝える！">
+            <input type="hidden" name="physicalCondition" value="" id="physicalCondition">
+            <input type="hidden" name="heartCondition"    value="" id="heartCondition">
         </div>        
     </div>
 </div>
@@ -24,12 +26,23 @@
 @section('script')
     <script>
         $('#yomerter').click(function (e){
+            //データに保持する為、ピンを置いた場所の相対位置を割合で取得する。
             var areaOffset = $('#yomerter').offset();
-            var offsetTop = ((e.pageY)-(areaOffset.top));
+            var offsetTop  = ((e.pageY)-(areaOffset.top));
             var offsetLeft = ((e.pageX)-(areaOffset.left));
+            let top        = Math.round(offsetTop  / $(this).height() * 100);
+            let left       = Math.round(offsetLeft / $(this).width()  * 100);
             
-            $('#pin').css('top',offsetTop);
-            $('#pin').css('left',offsetLeft);
+            //ピンのアニメーションの再起動の為、要素をCloneして挿入する。
+            var pin     = $('#pin'),
+            newpin      = pin.clone(true);
+            pin.before(newpin);
+            pin.remove();
+            $('#pin').css('top' ,top  + '%');
+            $('#pin').css('left',left + '%');
+            
+            $('#heartCondition').val(top);
+            $('#physicalCondition').val(left);
         });
     </script>
 @endsection
