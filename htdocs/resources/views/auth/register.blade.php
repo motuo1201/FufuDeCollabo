ed861ddb326ea1258a35d32f4ef33c41bba79a73
motuo1201 <enomotu@hotmail.com>
2018-01-29 14:42:54 +0900

    正確にエラーメッセージが出せるようにbladeを修正。

--- htdocs/resources/views/auth/register.blade.php
+++ htdocs/resources/views/auth/register.blade.php
@@ -72,43 +72,43 @@
         </div>
         <div class="col-xs-12 col-md-6 bg-primary" style="color:white;height:100vh" id="partner">
             <h2>パートナーの登録ページ</h2>
-            <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+            <div class="form-group{{ $errors->has('partners-name') ? ' has-error' : '' }}">
                 <label for="name" class="col-md-4 control-label">お名前</label>
 
                 <div class="col-md-6">
-                    <input id="partners-name" type="text" class="form-control" name="partners-name" value="{{ old('name') }}" required autofocus>
+                    <input id="partners-name" type="text" class="form-control" name="partners-name" value="{{ old('partners-name') }}" required autofocus>
 
                     @if ($errors->has('name'))
                         <span class="help-block">
-                            <strong>{{ $errors->first('name') }}</strong>
+                            <strong>{{ $errors->first('partners-name') }}</strong>
                         </span>
                     @endif
                 </div>
             </div>
 
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+            <div class="form-group{{ $errors->has('partners-email') ? ' has-error' : '' }}">
                 <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                 <div class="col-md-6">
-                    <input id="partners-email" type="email" class="form-control" name="partners-email" value="{{ old('email') }}" required>
+                    <input id="partners-email" type="email" class="form-control" name="partners-email" value="{{ old('partners-email') }}" required>
 
                     @if ($errors->has('email'))
                         <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
+                            <strong>{{ $errors->first('partners-email') }}</strong>
                         </span>
                     @endif
                 </div>
             </div>
 
-            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+            <div class="form-group{{ $errors->has('partners-password') ? ' has-error' : '' }}">
                 <label for="password" class="col-md-4 control-label">Password</label>
 
                 <div class="col-md-6">
                     <input id="partners-password" type="password" class="form-control" name="partners-password" required>
 
-                    @if ($errors->has('password'))
+                    @if ($errors->has('partners-password'))
                         <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
+                            <strong>{{ $errors->first('partners-password') }}</strong>
                         </span>
                     @endif
                 </div>
