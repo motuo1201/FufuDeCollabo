802c7b86d86240508da1cdb798486348f9967ffa
enomoto <y.enomoto@shuhai.co.jp>
2018-02-09 11:28:01 +0900

    viewの指定が誤っていたために修正 fixed #19

--- htdocs/app/Http/Controllers/Auth/TermsOfUseController.php
+++ htdocs/app/Http/Controllers/Auth/TermsOfUseController.php
@@ -8,6 +8,6 @@ use App\Http\Controllers\Controller;
 class TermsOfUseController extends Controller
 {
     public function index(){
-        return view('Auth.TermsOfUse');
+        return view('auth.TermsOfUse');
     }
 }
